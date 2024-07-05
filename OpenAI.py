dc8b4404cd270f46c4133906ae2e588a8ac3721a
AyushBhattad31 <ayush.bhattad@bradsol.com>
2024-07-05 13:21:10 +0530

    Open AI code updated

--- OpenAI.py
+++ OpenAI.py
@@ -21,7 +21,7 @@ def split_openai(text, initial_prompt):
             }
         ],
         "temperature": 0.7,
-        "max_tokens": 4050,
+        "max_tokens": 10000,
         "top_p": 1,
         "frequency_penalty": 0,
         "presence_penalty": 0
